09c0878d8ed6fe463d9ed11ad273e1635484476e
Frank <frankforflirt@gmail.com>
2025-08-20 16:48:33 +0530

    Banner Added

--- app/file/[id]/page.tsx
+++ app/file/[id]/page.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import '../../filePage.css';
 import Script from 'next/script';
 import DownloadInstructions from '../../components/DownloadInstructions';
+import AdBanner from '../../components/AdBanner';
 
 
 type Props = {
@@ -71,7 +72,9 @@ export default function FilePage({ params }: Props) {
             </div>
           )}
         </div>
+        <AdBanner/>
       <DownloadInstructions  fileType="video" />    
+ 
         {/* <a href={fileUrl} download className="download-button">
           <FileDown size={18} />
           <span>Download File</span>
