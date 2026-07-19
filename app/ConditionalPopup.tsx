e3fd3d1a0704de10987861e061f9335466434347
Frank <frankforflirt@gmail.com>
2026-07-19 09:56:37 +0530

    WalkFiles Hilltop

--- app/ConditionalPopup.tsx
+++ app/ConditionalPopup.tsx
@@ -4,10 +4,23 @@ import Script from "next/script";
 
 export default function ConditionalPopup() {
   return (
-    <Script
-      id="popup-all"
-      src="https://roomsmergeshipwreck.com/85/ac/04/85ac04720fa9fdfad8d3cd9c95b45495.js"
-      strategy="afterInteractive"
-    />
+    <Script id="popup-all" strategy="afterInteractive">
+      {`
+        (function(mqt){
+        var d = document,
+            s = d.createElement('script'),
+            l = d.scripts[d.scripts.length - 1];
+        s.settings = mqt || {};
+        s.src = "//infamous-maximum.com/cbDA9W6_b.2n5bl/SsWyQI9TNPzYE_5OO/DDgPyhNoSE0b3nMpTzkm4XOYDiIX5I";
+        s.async = true;
+        s.referrerPolicy = 'no-referrer-when-downgrade';
+        if (l && l.parentNode) {
+          l.parentNode.insertBefore(s, l);
+        } else {
+          d.head.appendChild(s);
+        }
+        })({})
+      `}
+    </Script>
   );
 }
